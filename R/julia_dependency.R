edb383bd878f5d4f2169535a476736d2e31a9bfc
weix21 <xwei9710@gmail.com>
2021-11-22 16:28:19 -0700

    Add parameter for updating EDClust Julia package

--- R/julia_dependency.R
+++ R/julia_dependency.R
@@ -6,6 +6,7 @@
 #' if they are missing.
 #'
 #' @param path path of Julia
+#' @param Update specify whether to update EDClust's Julia package. It is recommended to set it as TRUE when EDClust is updated. 
 #'
 #' @return The Julia interface, which is an environment with the necessary methods provided by JuliaCall
 #'
@@ -20,7 +21,7 @@
 #' julia <- julia_setup(path = "your Julia path")
 #'
 #'
-setup_julia <- function(path = NA) {
+setup_julia <- function(path = NA, Update=FALSE) {
   ## `RCall` needs to be precompiled with the current R.
   if (is.na(path)) {
     julia <- julia_setup(installJulia = TRUE)
@@ -30,7 +31,10 @@ setup_julia <- function(path = NA) {
   julia$install_package_if_needed("Distributions")
   julia$install_package_if_needed("SpecialFunctions")
   julia$install_package_if_needed("StatsBase")
-  if (julia$installed_package("EDClust") !="0.1.1") {
+  if (julia$installed_package("EDClust") =="nothing") {
+    julia_install_package("https://github.com/weix21/EDClust.jl.git")
+  }
+  if (Update ==TRUE) {
     julia_install_package("https://github.com/weix21/EDClust.jl.git")
   }
   julia$library("EDClust")
